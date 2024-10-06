64c81670ce88ecfed6c39364b2655d99c11af425
RedBoyBr <gustavo.da.costa.264@gmail.com>
2024-10-06 19:30:29 -0300

    Take Itens

--- PatonautasGame/Assets/Scripts/ArmMovement.cs
+++ PatonautasGame/Assets/Scripts/ArmMovement.cs
@@ -17,7 +17,15 @@ public class ArmMovement : MonoBehaviour
 
     private bool[] handsFree = new bool[2] {true, true};
 
+    private bool[] item = new bool[2];
+
     private Vector3[] hitPosition = new Vector3[2];
+
+    public LayerMask unmovableObjectLayer;
+
+    public LayerMask itemLayer;
+
+
 
     private void Start()
     {
@@ -35,7 +43,8 @@ public class ArmMovement : MonoBehaviour
 
         RaycastHit hitInfo;
 
-        Debug.DrawRay(transform.position, targetTargets[0], Color.red);
+        Debug.DrawRay(transform.position, hitPosition[0] - transform.position, Color.red);
+        Debug.DrawRay(transform.position, hitPosition[1] - transform.position, Color.green);
         //Movimento braço esquerdo
         if (handsFree[0])
         {
@@ -45,17 +54,27 @@ public class ArmMovement : MonoBehaviour
         {
             if (handsFree[0])
             {
-                if(Physics.Raycast(ray, out hitInfo, 1.5f))
+                if(Physics.Raycast(ray, out hitInfo, 1.5f, unmovableObjectLayer))
                 {
                     hitPosition[0] = hitInfo.point;
                     targetTargets[0] = hitInfo.point + hitInfo.normal * 0.1f;
                     rotates[0] = Quaternion.FromToRotation(Vector3.forward, -hitInfo.normal);
                     handsFree[0] = false;
                 }
+
+                if(Physics.Raycast(ray, out hitInfo, 1.5f, itemLayer))
+                {
+                    targetTargets[0] = hitInfo.point + hitInfo.normal * 0.1f;
+                    handsFree[0] = false;
+                    hitPosition[0] = hitInfo.point;
+                }
             }
             else
             {
-                handsFree[0] = true;
+                if (!item[0])
+                {
+                    handsFree[0] = true;
+                }
             }
         }
         //Movimento braço direiro
@@ -67,20 +86,32 @@ public class ArmMovement : MonoBehaviour
         {
             if (handsFree[1])
             {
-                if(Physics.Raycast(ray, out hitInfo, 1.5f))
+                if(Physics.Raycast(ray, out hitInfo, 1.5f, unmovableObjectLayer))
                 {
                     hitPosition[1] = hitInfo.point;
                     targetTargets[1] = hitInfo.point + hitInfo.normal * 0.1f;
                     rotates[1] = Quaternion.FromToRotation(Vector3.forward, -hitInfo.normal);
                     handsFree[1] = false;
                 } 
+
+                if(Physics.Raycast(ray, out hitInfo, 1.5f, itemLayer))
+                {
+                    targetTargets[1] = hitInfo.point + hitInfo.normal * 0.1f;
+                    handsFree[1] = false;
+                    hitPosition[1] = hitInfo.point;
+                }
             }
             else
             {
-                handsFree[1] = true;
+                if (!item[1])
+                {
+                    handsFree[1] = true;
+                }
+
             }
         }
 
+        //pulo
         if (Input.GetKeyDown(KeyCode.Space))
         {
             float timePassed = 0;
@@ -102,33 +133,65 @@ public class ArmMovement : MonoBehaviour
         targets[1].position = Vector3.Lerp(targets[1].position, targetTargets[1], 0.5f * Time.deltaTime * 5);
 
         //Rotação esquerda
-        if (!handsFree[0])
+        if (!handsFree[0]) //Rotação agarrada
         {
-            if(Physics.Raycast(rayT1, out hitInfo, 1.5f))
+            if (item[0])
+            {
+                targetTargets[0] = offSets[0].position;
+            }
+            if(Physics.Raycast(rayT1, out hitInfo, 1.5f, unmovableObjectLayer))
             {
                 targetTargets[0] = hitInfo.point + hitInfo.normal * 0.1f;
-                
+            }
+            if(Physics.Raycast(rayT1, out hitInfo,1.5f, itemLayer))
+            {
+                if ((hitInfo.point - targets[0].position).magnitude <= 0.2)
+                {
+                    hitInfo.transform.SetParent(targets[0]);
+                    item[0] = true;
+                }
             }
             rotates[0] = Quaternion.FromToRotation(Vector3.forward, -hitInfo.normal);
             targets[0].rotation = Quaternion.Lerp(targets[0].rotation, rotates[0], 0.5f * Time.deltaTime * 5);
         }
-        else
+        else //Rotação presa
         {
             targets[0].rotation = Quaternion.Lerp(targets[0].rotation, offSets[0].rotation, 0.5f * Time.deltaTime * 5);
         }
         //Rotação direita
-        if (!handsFree[1])
+        if (!handsFree[1]) //Rotação agarrada
         {
-            if(Physics.Raycast(rayT2, out hitInfo, 1.5f))
+            if (item[1])
+            {
+                targetTargets[1] = offSets[1].position;
+            }
+
+            if(Physics.Raycast(rayT2, out hitInfo, 1.5f, unmovableObjectLayer))
             {
                 targetTargets[1] = hitInfo.point + hitInfo.normal * 0.1f;
+            }
+            if(Physics.Raycast(rayT2, out hitInfo, 1.5f, itemLayer))
+            {
+                if ((hitInfo.point - targets[1].position).magnitude <= 0.2)
+                {
+                    hitInfo.transform.SetParent(targets[1]);
+                    item[1] = true;
+                }
             }
             rotates[1] = Quaternion.FromToRotation(Vector3.forward, -hitInfo.normal);
             targets[1].rotation = Quaternion.Lerp(targets[1].rotation, rotates[1], 0.5f * Time.deltaTime * 5);
         }
-        else
+        else //Rotação presa
         {
             targets[1].rotation = Quaternion.Lerp(targets[1].rotation, offSets[1].rotation, 0.5f * Time.deltaTime * 5);
         }
+    }
+    public void Soltar(GameObject itemObj)
+    {
+        Destroy(itemObj);
+        item[0] = false;
+        item[1] = false;
+        handsFree[1] = true;
+        handsFree[0] = true;
     }
 }
